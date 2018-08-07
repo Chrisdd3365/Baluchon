a9d73ce153a397f951a58f64cf1faaab8fe197c8
Chrisdd3365 <32330136+Chrisdd3365@users.noreply.github.com>
2018-08-07 15:46:48 +0200

    Initial Commit

---
+++ Baluchon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Baluchon
+//
+//  Created by Christophe DURAND on 07/08/2018.
+//  Copyright © 2018 Christophe DURAND. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
