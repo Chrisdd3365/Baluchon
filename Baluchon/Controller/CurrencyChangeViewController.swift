6fd2e1bf9e1579f463fcfddb740e24adc3447a5b
Chrisdd3365 <32330136+Chrisdd3365@users.noreply.github.com>
2018-08-07 19:38:18 +0200

    design
    trying to design the first page

---
+++ Baluchon/Controller/CurrencyChangeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CurrencyChangeViewController.swift
+//  Baluchon
+//
+//  Created by Christophe DURAND on 07/08/2018.
+//  Copyright © 2018 Christophe DURAND. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyChangeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
