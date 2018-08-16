bd9daa75baf4d1fd9e8256b7afe8da5c22839200
Chrisdd3365 <32330136+Chrisdd3365@users.noreply.github.com>
2018-08-16 23:06:44 +0200

    appel réseau fixer api

--- Baluchon/Controller/CurrencyChangeViewController.swift
+++ Baluchon/Controller/CurrencyChangeViewController.swift
@@ -9,8 +9,80 @@
 import UIKit
 
 class CurrencyChangeViewController: UIViewController {
+    
+    var currencyChangeService = CurrencyChangeService()
+    var selectedCurrency: Double = 0
 
+    @IBOutlet weak var myValueTextField: UITextField!
+    @IBOutlet weak var myCurrencyTextField: UITextField!
+    @IBOutlet weak var myConvertedValueLabel: UILabel!
+    @IBOutlet weak var currencyPickerView: UIPickerView!
+    
+    @IBAction func convert(_ sender: Any) {
+      
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        createCurrencyPicker()
+        createNumberPad()
+        createToolBar()
+    }
+    
+    func createCurrencyPicker() {
+        let currencyPicker = UIPickerView()
+        currencyPicker.delegate = self
+        myCurrencyTextField.inputView = currencyPicker
+    }
+    
+    func createNumberPad() {
+        myValueTextField.keyboardType = UIKeyboardType.numberPad
+    }
+    
+    func createToolBar() {
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(CurrencyChangeViewController.dismissKeyboard))
+        toolBar.setItems([doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        myValueTextField.inputAccessoryView = toolBar
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    private func showAlert() {
+        let alertVC = UIAlertController(title: "Error", message: "The conversion failed.", preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        present(alertVC, animated: true, completion: nil)
     }
 }
+
+extension CurrencyChangeViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currencyChangeService.myCurrency.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return currencyChangeService.myCurrency[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedCurrency = currencyChangeService.myValues[row]
+    }
+}
+
+
+
+
+
+
+
+
+
+
--- Baluchon/Model/CurrencyChangeService.swift
+++ Baluchon/Model/CurrencyChangeService.swift
@@ -10,39 +10,21 @@ import UIKit
 
 class CurrencyChangeService {
     
+    var myCurrency:[String] = []
+    var myValues:[Double] = []
+    
+    var activeCurrency:Double = 0
+    
     private static let currencyChangeUrl = URL(string: "http://data.fixer.io/api/latest?access_key=2e4288a2049e923be5767c9bacf7ae2a")!
     
-    static func getQuote() {
-        let request = createCurrencyChangeRequest()
-        let session = URLSession(configuration: .default)
+    func getCurrencyChange() {
         
-        let task = session.dataTask(with: request) { (data, response, error) in
-            if let data = data, error == nil {
-                if let response = response as? HTTPURLResponse, response.statusCode == 200 {
-                    if let responseJSON = try? JSONDecoder().decode([String: String].self, from: data),
-                        let currencyRates = responseJSON["rates"] {
-                        print(currencyRates)
-                    }
-                }
-            }
-        }
-        task.resume()
+        
+        
+        
     }
-    
-    
-    
-   private static func createCurrencyChangeRequest() -> URLRequest {
-        var request = URLRequest(url: currencyChangeUrl)
-        request.httpMethod = "GET"
         
-        let body = "method=getQuote&lang=en&format=json"
-        request.httpBody = body.data(using: .utf8)
         
-        return request
-    }
-    
-    
-    
-    
-    
+        
+        
 }
