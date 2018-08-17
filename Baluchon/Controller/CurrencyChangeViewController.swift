f5681745f8eaf4df44f8a73bfdf78c4b50e022f4
Chrisdd3365 <32330136+Chrisdd3365@users.noreply.github.com>
2018-08-17 19:39:44 +0200

    trying to fix network request for fixer api

--- Baluchon/Controller/CurrencyChangeViewController.swift
+++ Baluchon/Controller/CurrencyChangeViewController.swift
@@ -11,21 +11,22 @@ import UIKit
 class CurrencyChangeViewController: UIViewController {
     
     var currencyChangeService = CurrencyChangeService()
-    var selectedCurrency: Double = 0
-
+    var activeCurrency:Double = 0
+    
     @IBOutlet weak var myValueTextField: UITextField!
     @IBOutlet weak var myCurrencyTextField: UITextField!
     @IBOutlet weak var myConvertedValueLabel: UILabel!
-    @IBOutlet weak var currencyPickerView: UIPickerView!
     
     @IBAction func convert(_ sender: Any) {
-      
+
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        currencyChangeService.getCurrencyChange()
         createCurrencyPicker()
-        createNumberPad()
+//        createNumberPad()
         createToolBar()
     }
     
@@ -35,9 +36,9 @@ class CurrencyChangeViewController: UIViewController {
         myCurrencyTextField.inputView = currencyPicker
     }
     
-    func createNumberPad() {
-        myValueTextField.keyboardType = UIKeyboardType.numberPad
-    }
+//    func createNumberPad() {
+//        myValueTextField.keyboardType = UIKeyboardType.numberPad
+//    }
     
     func createToolBar() {
         let toolBar = UIToolbar()
@@ -63,17 +64,17 @@ extension CurrencyChangeViewController: UIPickerViewDelegate, UIPickerViewDataSo
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
-    
+
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return currencyChangeService.myCurrency.count
     }
-    
+
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return currencyChangeService.myCurrency[row]
     }
-    
+
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        selectedCurrency = currencyChangeService.myValues[row]
+        activeCurrency = currencyChangeService.myValues[row]
     }
 }
 
--- Baluchon/Model/CurrencyChangeService.swift
+++ Baluchon/Model/CurrencyChangeService.swift
@@ -13,18 +13,45 @@ class CurrencyChangeService {
     var myCurrency:[String] = []
     var myValues:[Double] = []
     
-    var activeCurrency:Double = 0
-    
-    private static let currencyChangeUrl = URL(string: "http://data.fixer.io/api/latest?access_key=2e4288a2049e923be5767c9bacf7ae2a")!
+    let currencyChangeUrl = URL(string: "http://data.fixer.io/api/latest?access_key=2e4288a2049e923be5767c9bacf7ae2a&callback=getCurrencyChange")!
     
     func getCurrencyChange() {
-        
-        
-        
+        let task = URLSession.shared.dataTask(with: currencyChangeUrl) { (data, response, error) in
+            
+            if error != nil
+            {
+                print ("ERROR")
+            }
+            else
+            {
+                if let content = data
+                {
+                    do
+                    {
+                        let myJson = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                        
+                        if let rates = myJson["rates"] as? NSDictionary
+                        {
+                            for (key, value) in rates
+                            {
+                                self.myCurrency.append((key as? String)!)
+                                self.myValues.append((value as? Double)!)
+                            }
+                        }
+                    }
+                    catch
+                    {
+                        
+                    }
+                }
+            }
+        }
+        task.resume()
         
     }
-        
-        
-        
-        
+    
+    
+    
+    
+    
 }
